765e492a734e296ec2a0067d58b29745334c85ba
Omkar Magadum <omkar.magadum.dev@gmail.com>
2026-07-31 01:34:19 -0600

    feat: add UseReducerHook component for state management with useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Counter from './components/Counter'
 import UseEffect from './components/use_effect_hook'
+import UseReducerHook from './components/use_reducer_hook'
 import UseStateHook from './components/use_state_hook'
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
       <h2>Hooks Interview Questions in React</h2>
       {/* <UseStateHook /> */}
       {/* <UseEffect /> */}
-      <Counter />
+      {/* <Counter /> */}
+      <UseReducerHook />
     </div>
   )
 }
