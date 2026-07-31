31cde7dad38e6f2d02de928fdef4bce1f2c3970f
Omkar Magadum <omkar.magadum.dev@gmail.com>
2026-07-31 09:04:53 -0600

    feat: implement UseMemoAndCallbackHook component for memoization and performance optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Counter from './components/Counter'
+import UseMemoAndCallbackHook from './components/use-memo-and-context-hook'
 import UseEffect from './components/use_effect_hook'
 import UseReducerHook from './components/use_reducer_hook'
 import UseStateHook from './components/use_state_hook'
@@ -13,7 +14,8 @@ function App() {
       {/* <UseStateHook /> */}
       {/* <UseEffect /> */}
       {/* <Counter /> */}
-      <UseReducerHook />
+      {/* <UseReducerHook /> */}
+      <UseMemoAndCallbackHook />
     </div>
   )
 }
